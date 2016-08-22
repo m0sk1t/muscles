e7bafc32455f6877131e8d9ec2afa27bdc748f22
m0sk1t <m0sk1t>
2016-08-23 02:51:24 +0300

    dialogs started

--- routes/dialogs_route.js
+++ routes/dialogs_route.js
@@ -1,45 +1,72 @@
 module.exports = (app) => {
 	var mongoose = require('mongoose'),
 		Dialogs = mongoose.model('Dialogs', {
-			creDate: Date,
 			users: Array,
+			creDate: Date,
+			deleters: Array,
 			messages: Array,
 		});
 	app.route('/dialog/:id')
 		.get((req, res) => {
-			if (req.params.id === 'all') {
-				Users.findOne({
-					userid: req.cookies.userid
-				}, (err, user) => {
-					Dialogs.find({
-						users: user._id,
-					}, {
-						users: 1,
-						creDate: 1,
-					}, (err, dialogs) => {
-						res.json(dialogs);
-					});
+			Users.findOne({
+				userid: req.cookies.userid
+			}, (err, user) => {
+				Dialogs.find({
+					users: user._id,
+					deleters: {
+						$ne: user._id
+					}
+				}, {
+					users: 1,
+					creDate: 1,
+					messages: 1,
+				}, (err, dialogs) => {
+					res.json(dialogs);
 				});
-			} else {
-				Dialogs.findById(req.params.id, (err, dialog) => {
-					res.json(dialog);
-				});
-			}
+			});
 		})
 		.put((req, res) => {
-			delete req.body._id;
-			delete req.body.__v;
-			Dialogs.findByIdUpdate(req.params.id, {
-				$set: req.body
-			}, {
-				upsert: false
-			}, (err, dialog) => {
-				res.json(dialog);
+			Users.findOne({
+				userid: req.cookies.userid
+			}, (err, user) => {
+				Dialogs.findOneAndUpdate({
+					users: user._id,
+					_id: req.params.id,
+				}, {
+					$addToSet: {
+						messages: req.body.message
+					}
+				}, (err, dialog) => {
+					res.json(dialog);
+				});
 			});
 		})
 		.delete((req, res) => {
-			Dialogs.findByIdAndRemove(req.params.id, (err, dialog) => {
+			Users.findOne({
+				userid: req.cookies.userid
+			}, (err, user) => {
+				Dialogs.findOneAndUpdate({
+					_id: req.params.id,
+					users: user._id
+				}, {
+					$addToSet: {
+						deleters: user._id
+					}
+				}, (err, dialog) => {
+					res.json(dialog);
+				});
+			});
+		});
+	app.post('/dialog', (req, res) => {
+		Users.findOne({
+			userid: req.cookies.userid
+		}, (err, user) => {
+			Dialogs.create({
+				messages: [req.body.message],
+				users: [user._id, req.body.addressee],
+			}, (err, dialog) => {
 				res.json(dialog);
 			});
 		});
+	});
 };
