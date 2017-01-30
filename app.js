59131cca1e8fe56c2c3e425308afc2b85bd24d89
m0sk1t <m0sk1t@bk.ru>
2017-01-30 14:18:27 +0300

    Rearrange buttons

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 app.use(cookieParser());
 
